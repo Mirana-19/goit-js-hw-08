40f764880958071b45d29a506bac5d086d24f429
Marta Żak <martysik19@gmail.com>
2022-02-14 03:57:22 +0100

    1st task solution added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,37 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('.gallery');
+
+function addGalleryMarkup(images) {
+  const markup = images.map(image => createGalleryMarkup(image)).join('');
+
+  return (galleryRef.innerHTML = markup);
+}
+
+function createGalleryMarkup(image) {
+  const { original, preview, description } = image;
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}
+
+addGalleryMarkup(galleryItems);
+
+galleryRef.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
