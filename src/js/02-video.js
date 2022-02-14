69bef0cd7dbca7aade1a95481543348ade01a961
Marta Żak <martysik19@gmail.com>
2022-02-14 17:55:39 +0100

    second task solution added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+var throttle = require('lodash.throttle');
 
+player.on(
+  'timeupdate',
+  throttle(function (currentTime) {
+    localStorage.setItem('videoplayer-current-time', `${JSON.stringify(currentTime)}`);
+  }, 1000),
+);
+
+const videoplayerCurrentTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+if (videoplayerCurrentTime.seconds >= 560) {
+  player.setCurrentTime(0); //сделала рестарт видео с 0, если посмотрели почти до конца
+} else {
+  player.setCurrentTime(videoplayerCurrentTime.seconds);
+}
