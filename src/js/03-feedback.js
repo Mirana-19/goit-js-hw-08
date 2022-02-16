07669cba593085060de5b4bbacb461e875abade4
Marta Żak <martysik19@gmail.com>
2022-02-16 16:16:01 +0100

    resolved issues in task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,13 +5,16 @@ const emailInputRef = document.querySelector('[name="email"]');
 const messageInputRef = document.querySelector('[name="message"]');
 const PROP_KEY = 'feedback-form-state';
 const parsedLocalStorage = JSON.parse(localStorage.getItem(PROP_KEY));
+const data = {};
 
 formRef.addEventListener('input', throttle(handleInput), 500);
-// не могу понять, почему не работает throttle, подскажите, пожалуйста, мучаюсь уже долго :)
+formRef.addEventListener('submit', handleSubmit);
 
 function checkLocalStorage() {
-  if (localStorage.getItem(PROP_KEY)) {
+  if (parsedLocalStorage?.email) {
     emailInputRef.value = parsedLocalStorage.email;
+  }
+  if (parsedLocalStorage?.message) {
     messageInputRef.value = parsedLocalStorage.message;
   }
 }
@@ -19,18 +22,18 @@ function checkLocalStorage() {
 checkLocalStorage();
 
 function handleInput(event) {
-  const {
-    elements: { email, message },
-  } = event.currentTarget;
-
-  const savedMessage = { email: email.value, message: message.value };
-
-  localStorage.setItem(PROP_KEY, JSON.stringify(savedMessage));
-  console.log(localStorage);
+  data[event.target.name] = event.target.value;
+  localStorage.setItem(PROP_KEY, JSON.stringify(data));
 }
 
 function handleSubmit(event) {
   event.preventDefault();
+
+  if (emailInputRef.value === '' || messageInputRef.value === '') {
+    alert('Заполните, пожалуйста, все поля!');
+  }
+  console.log(`E-mail: ${parsedLocalStorage.email}, Message: ${parsedLocalStorage.message}`);
+
   localStorage.removeItem(PROP_KEY);
   formRef.reset();
 }
